b3bc95ab2511af7fe6827c94630cf1945ab93305
lndsilva <lndsilva@hotmail.com>
2021-06-30 11:30:51 -0300

    implementação do fragmentManager

--- app/src/main/java/br/com/local/appti93_sistema/PageAdapterPets.java
+++ app/src/main/java/br/com/local/appti93_sistema/PageAdapterPets.java
@@ -9,20 +9,40 @@ import org.jetbrains.annotations.NotNull;
 
 public class PageAdapterPets extends FragmentPagerAdapter {
 
+    //Criar uma variável global para informar a quantidade de itens no tablayout
+    private int tabCount;
 
-    public PageAdapterPets(@NonNull @NotNull FragmentManager fm, int behavior) {
-        super(fm, behavior);
+    public PageAdapterPets(@NonNull @NotNull FragmentManager fm, int tabCount) {
+        super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+
+        this.tabCount = tabCount;
+
     }
 
     @NonNull
     @NotNull
     @Override
     public Fragment getItem(int position) {
+
+        switch (position){
+            case 0:
+                return new CachorroFragment();
+            case 1:
+                return new GatoFragment();
+            case 2:
+                return new PeixeFragment();
+        }
+
         return null;
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return tabCount;
+    }
+
+    @Override
+    public int getItemPosition(@NonNull @NotNull Object object) {
+        return POSITION_NONE;
     }
 }
--- app/src/main/java/br/com/local/appti93_sistema/PetsActivity.java
+++ app/src/main/java/br/com/local/appti93_sistema/PetsActivity.java
@@ -1,20 +1,34 @@
 package br.com.local.appti93_sistema;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class PetsActivity extends AppCompatActivity {
     MaterialToolbar idToolBarpets;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    TabItem item_cachorro, item_gato, item_peixe;
+    PageAdapterPets adapterPets;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pets_layout);
+
+        tabLayout = findViewById(R.id.idTabPets);
+        viewPager = findViewById(R.id.idViewPagerPets);
+        item_cachorro = findViewById(R.id.idTabItemCachorro);
+        item_gato = findViewById(R.id.idTabItemGato);
+        item_peixe = findViewById(R.id.idTabItemPeixe);
 
         idToolBarpets = findViewById(R.id.idToolBarpets);
 
@@ -26,5 +40,36 @@ public class PetsActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+
+        adapterPets = new PageAdapterPets(getSupportFragmentManager(),tabLayout.getTabCount());
+
+        //Ligar o viewPager ao adaptador
+        viewPager.setAdapter(adapterPets);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                //posição da seleção atual
+                viewPager.setCurrentItem(tab.getPosition());
+                //status da seleção para o adapterPages
+                adapterPets.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        //implementando o viewPager ao fragment
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+
     }
 }
